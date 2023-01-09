036af63dca5239bf328eab711895992cf3d40e28
marcosmoraesnf <86791856+marcosmoraesnf@users.noreply.github.com>
2023-01-09 17:02:57 -0300

    gerando botões e funções para montar o hamburguer

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,28 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredients/BurgerIngredient";
 
 const burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey} />;
+    });
+  });
+
+  const haveIngredients = transformedIngredients.reduce((acumulator, array) => {
+    return acumulator + array.length;
+  }, 0);
+
+  if (haveIngredients === 0) {
+    transformedIngredients = (
+      <div>
+        <h1>Please add some ingredients!</h1>
+      </div>
+    );
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="salad" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
-      <BurgerIngredient type="bacon" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,109 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import Aux from "../../hoc/Auxiliary";
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+import Modal from "../../components/UI/Modal/Modal";
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 
-const burguerBuilder = (props) => {
+const INGREDIENTS_PRICE = {
+  salad: 0.5,
+  bacon: 0.7,
+  cheese: 0.4,
+  meat: 1.3,
+};
+
+const BurguerBuilder = (props) => {
+  const [ingredients, setIngredients] = useState({
+    salad: 0,
+    bacon: 0,
+    cheese: 0,
+    meat: 0,
+  });
+
+  const [totalPrice, setTotalPrice] = useState(4);
+
+  const [purchasable, setPurchasable] = useState(false);
+  const [purchasing, setPurchasing] = useState(false);
+
+  const purchaseHandler = () => {
+    setPurchasing(true);
+  };
+
+  const purchaseCancelHandler = () => {
+    setPurchasing(false);
+  };
+
+  const updatePurchase = (ingredients) => {
+    const sum = Object.keys(ingredients)
+      .map((igKey) => {
+        return ingredients[igKey];
+      })
+      .reduce((acc, igQnty) => {
+        return acc + igQnty;
+      }, 0);
+
+    setPurchasable(sum > 0);
+  };
+
+  const addIngredientsHandler = (type) => {
+    const oldCount = ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+      ...ingredients,
+    };
+    updatedIngredients[type] = updatedCount;
+
+    const priceAddition = INGREDIENTS_PRICE[type];
+    const oldPrice = totalPrice;
+    const newPrice = oldPrice + priceAddition;
+
+    setIngredients(updatedIngredients);
+    setTotalPrice(newPrice);
+    updatePurchase(updatedIngredients);
+  };
+
+  const removeIngredientsHandler = (type) => {
+    const oldCount = ingredients[type];
+    const updatedCount = oldCount - 1;
+    if (oldCount <= 0) {
+      return;
+    }
+    const updatedIngredients = {
+      ...ingredients,
+    };
+    updatedIngredients[type] = updatedCount;
+
+    const priceDeduction = INGREDIENTS_PRICE[type];
+    const oldPrice = totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+
+    setIngredients(updatedIngredients);
+    setTotalPrice(newPrice);
+    updatePurchase(updatedIngredients);
+  };
+
+  const disabledInfo = { ...ingredients };
+
+  for (let key in disabledInfo) {
+    disabledInfo[key] = disabledInfo[key] <= 0;
+  }
+
   return (
     <Aux>
-      <Burger />
-      <div>Build Controls</div>
+      <Modal show={purchasing} modalClosed={purchaseCancelHandler}>
+        <OrderSummary ingredients={ingredients} />
+      </Modal>
+      <Burger ingredients={ingredients} />
+      <BuildControls
+        addIngredients={addIngredientsHandler}
+        removeIngredients={removeIngredientsHandler}
+        disabled={disabledInfo}
+        price={totalPrice}
+        purchasable={purchasable}
+        ordered={purchaseHandler}
+      />
     </Aux>
   );
 };
 
-export default burguerBuilder;
+export default BurguerBuilder;
