8be05b9aa5c20726ac7550251064debb677c4faf
marcosmoraesnf <86791856+marcosmoraesnf@users.noreply.github.com>
2023-01-23 17:03:02 -0300

    inserindo spinner condicional enquanto a requisição é realizada

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,10 +3,12 @@ import { useLocation, useNavigate } from "react-router-dom";
 import Button from "../../../components/UI/Button/Button";
 import classes from "./ContactData.css";
 import axios from "../../../axiosOrders";
+import Spinner from "../../../components/UI/Spinner/Spinner";
 
 const ContactData = () => {
   const { state } = useLocation();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   const [userData, setUserData] = useState({
     name: "",
@@ -39,55 +41,65 @@ const ContactData = () => {
    */
 
   const submitOrderHandler = () => {
+    setLoading(true);
     axios
       .post("/orders.json", order)
       .then((response) => {
-        /*navigate("/checkout/contact-data", {
+        navigate("/", {
           state: {
             ingredients: state.ingredients,
             totalPrice: state.totalPrice,
           },
-        });*/
+        });
         //setLoading(false);
         //setPurchasing(false);
+        setLoading(false);
         console.log(response);
       })
       .catch((error) => {
         //setLoading(false);
         //setPurchasing(false);
+        setLoading(false);
         console.log(error);
       });
   };
 
+  let form = (
+    <form>
+      <input
+        className={classes.Input}
+        type="text"
+        name="name"
+        placeholder="Your Name"
+      />
+      <input
+        className={classes.Input}
+        type="text"
+        name="email"
+        placeholder="Your Email"
+      />
+      <input
+        className={classes.Input}
+        type="text"
+        name="street"
+        placeholder="Street"
+      />
+      <input
+        className={classes.Input}
+        type="text"
+        name="zipCode"
+        placeholder="Z-code"
+      />
+    </form>
+  );
+  if (loading) {
+    form = <Spinner />;
+  }
+
   return (
     <div className={classes.ContactData}>
       <h4>Entry your contact data</h4>
-      <form>
-        <input
-          className={classes.Input}
-          type="text"
-          name="name"
-          placeholder="Your Name"
-        />
-        <input
-          className={classes.Input}
-          type="text"
-          name="email"
-          placeholder="Your Email"
-        />
-        <input
-          className={classes.Input}
-          type="text"
-          name="street"
-          placeholder="Street"
-        />
-        <input
-          className={classes.Input}
-          type="text"
-          name="zipCode"
-          placeholder="Z-code"
-        />
-      </form>
+      {form}
       <Button btnType="Success" clicked={submitOrderHandler}>
         ORDER
       </Button>
