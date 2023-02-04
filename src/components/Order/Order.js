e0b87c452856cd7a2caa909bf012070827a70988
marcosmoraesnf <86791856+marcosmoraesnf@users.noreply.github.com>
2023-02-04 19:18:36 -0300

    ajustando estilos dos componentes

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -4,11 +4,13 @@ import classes from "./Order.css";
 const Order = (props) => {
   return (
     <div className={classes.Order}>
-      <h2>Ingredients:</h2>
-      <p>
-        Salad: {props.salad} Bacon: {props.bacon} Meat: {props.meat} Cheese:{" "}
-        {props.cheese}
-      </p>
+      <h2 className={classes.Emphasis}>Ingredients:</h2>
+      <ul>
+        <li> Salad: {props.salad}</li>
+        <li> Bacon: {props.bacon}</li>
+        <li> Meat: {props.meat}</li>
+        <li> Cheese:{props.cheese} </li>
+      </ul>
       <p>
         Total price: <strong>$ {props.price}</strong>
       </p>
