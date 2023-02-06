870230d24a1682c28e9908c372ba2db762cc776d
marcosmoraesnf <86791856+marcosmoraesnf@users.noreply.github.com>
2023-02-06 10:30:16 -0300

    Inserindo Lazy Loading nas páginas possíveis

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import Layout from "./hoc/Layout/Layout";
 import BurguerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import Checkout from "./containers/Checkout/Checkout";
-import Orders from "./containers/Orders/Orders";
-import Auth from "./containers/Auth/Auth";
+//import Checkout from "./containers/Checkout/Checkout";
+//import Orders from "./containers/Orders/Orders";
+//import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
+import React, { Suspense } from "react";
 
 function App() {
   const token = useSelector((state) => state.token.tokenId);
+  const LazyAuth = React.lazy(() => import("./containers/Auth/Auth"));
+  const LazyOrders = React.lazy(() => import("./containers/Orders/Orders"));
+  const LazyCheckout = React.lazy(() =>
+    import("./containers/Checkout/Checkout")
+  );
 
   let routes = (
     <>
       <Route path="/" exact element={<BurguerBuilder />} />
-      <Route path="/sign-in" exact element={<Auth />} />
+      <Route path="/sign-in" exact element={<LazyAuth />} />
     </>
   );
   if (token) {
     routes = (
       <>
         <Route path="/" exact element={<BurguerBuilder />} />
-        <Route path="/checkout/*" exact element={<Checkout />} />
-        <Route path="/orders" exact element={<Orders />} />
+        <Route path="/checkout/*" exact element={<LazyCheckout />} />
+        <Route path="/orders" exact element={<LazyOrders />} />
         <Route path="/logout" exact element={<Logout />} />
       </>
     );
@@ -31,7 +37,9 @@ function App() {
     <div>
       <BrowserRouter>
         <Layout>
-          <Routes>{routes}</Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>{routes}</Routes>
+          </Suspense>
         </Layout>
       </BrowserRouter>
     </div>
